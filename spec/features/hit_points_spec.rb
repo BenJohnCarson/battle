c0c7dcbba359f75191607fea7d3a86831a5002c7
Benjamin Carson <benjohncarson@gmail.com>
2017-01-18 11:38:31 +0000

    test refactor

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'view hit points' do
     scenario 'shows player 2 hit points' do
-        visit('/')
-        fill_in :player_1_name, with: 'Sam'
-	    fill_in :player_2_name, with: 'Ben'
-	    click_button 'Submit'
-	    expect(page).to have_content 'Ben: 100hp'
+	   sign_in_and_play
+	   expect(page).to have_content 'Ben: 100hp'
 	end
 end
